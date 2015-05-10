16f762331f0afc836015ed421d797a7fb7e58466
pigsdofly <pigsdofly6@gmail.com>
2015-05-10 02:55:38 +0100

    heroku lol

--- local_settings.py
+++ local_settings.py
@@ -1,12 +1,12 @@
 #put API keys here
-pub_key = ""
-priv_key =  ""
-access_key = ""
-secret_key = ""
+pub_key = "BZ6ITPXyHck7TQZTnvJmFjfnz"
+priv_key =  "FYERRmQkiDCNdFv6w96ErDUdZgPfXTKA2n1sCzjuNbryfK8GOt"
+access_key = "3241892897-2BxdVexT1TpV6pKbeQWY4ZmJUFUafXF4ayT4tj2"
+secret_key = "rHaWUgrhJcqr0T8PavBj0jvNWQA8xF1bodxdKI8QzlFLu"
 
 #put username (@) of account here 
-source = ""
+source = "sanae_imagebot"
 
 #put danbooru tags here
-tags = ["",""]
+tags = ["kochiya_sanae","rating:s"]
 
